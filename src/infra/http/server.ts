050911ae3040425de84996b2c1e10457cea8dca3
Thais Cristine <this.cristine06@gmail.com>
2026-03-22 15:23:15 +0200

    add postgresql with docker

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,9 +1,12 @@
 import { fastifyCors } from '@fastify/cors'
 import { fastify } from 'fastify'
+import { env } from '@/env'
 
 const server = fastify()
 
 server.register(fastifyCors, { origin: '*' })
+
+console.log(env.DATABASE_URL)
 
 server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('HTTP Server is running')
