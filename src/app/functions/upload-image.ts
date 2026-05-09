e3c509f1aa488167b32effa16d2b4f740aa13113
Thais Cristine <this.cristine06@gmail.com>
2026-05-10 00:18:20 +0300

    Handle errors individually (functional) - similar to Go language

--- src/app/functions/upload-image.ts
+++ src/app/functions/upload-image.ts
@@ -1,31 +1,43 @@
-import { db } from '@/infra/db';
-import { schema } from '@/infra/db/schemas';
-import { Readable } from 'node:stream';
-import { z } from 'zod';
+import { Readable } from 'node:stream'
+import { z } from 'zod'
+import { db } from '@/infra/db'
+import { schema } from '@/infra/db/schemas'
+import { type Either, makeLeft, makeRight } from '@/shared/either'
+import { InvalidFileFormatError } from './errors/invalid-file-format'
 
 const uploadImageInput = z.object({
-    fileName: z.string(),
-    contentType: z.string(),
-    contentStream: z.instanceof(Readable)
-});
+  fileName: z.string(),
+  contentType: z.string(),
+  contentStream: z.instanceof(Readable),
+})
 
-type UploadImageInput = z.input<typeof uploadImageInput>;
+type UploadImageInput = z.input<typeof uploadImageInput>
 
-const allowedMimeTypes = ['image/jpeg', 'image/jpg', 'image/png', 'image/gif', 'image/webp'];
+const allowedMimeTypes = [
+  'image/jpeg',
+  'image/jpg',
+  'image/png',
+  'image/gif',
+  'image/webp',
+]
 
-export async function uploadImage(input: UploadImageInput){
-    const { fileName, contentType, contentStream } = uploadImageInput.parse(input);
+export async function uploadImage(
+  input: UploadImageInput
+): Promise<Either<InvalidFileFormatError, { url: string }>> {
+  const { fileName, contentType, contentStream } = uploadImageInput.parse(input)
 
-    if (!allowedMimeTypes.includes(contentType)) {
-        throw new Error('Unsupported file type');
-    }
-    
-    //TODO: upload the image to Cloudflare R2
-    console.log(`Uploading file: ${fileName} with content type: ${contentType}`);
+  if (!allowedMimeTypes.includes(contentType)) {
+    return makeLeft(new InvalidFileFormatError())
+  }
 
-    await db.insert(schema.uploads).values({
-        name: fileName,
-        remoteKey: fileName,
-        remoteUrl: fileName,
-    })
+  //TODO: upload the image to Cloudflare R2
+  console.log(`Uploading file: ${fileName} with content type: ${contentType}`)
+
+  await db.insert(schema.uploads).values({
+    name: fileName,
+    remoteKey: fileName,
+    remoteUrl: fileName,
+  })
+
+  return makeRight({ url: '' })
 }
--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -1,8 +1,9 @@
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import z from 'zod'
+import { uploadImage } from '@/app/functions/upload-image'
 import { db } from '@/infra/db'
 import { schema } from '@/infra/db/schemas'
-import { uploadImage } from '@/app/functions/upload-image'
+import { isRight, unwrapEither } from '@/shared/either'
 
 export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
   server.post(
@@ -11,18 +12,8 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
       schema: {
         summary: 'Upload an image',
         consumes: ['multipart/form-data'],
-        // body: z.object({
-        //   name: z.string().min(2).max(100).describe('The name of the image'),
-        //   password: z
-        //     .string()
-        //     .optional()
-        //     .describe('The password for the image'),
-        //   file: z.string().min(1).max(100).describe('The image file to upload'),
-        // }),
         response: {
-          201: z.object({
-            uploadId: z.string().describe('The ID of the uploaded image'),
-          }),
+          201: z.null().describe('Image uploaded successfully'),
           400: z.object({
             message: z.string().describe('File is required'),
           }),
@@ -36,22 +27,26 @@ export const uploadImageRoute: FastifyPluginAsyncZod = async server => {
         },
       })
 
-      if(!uploadedFile) {
+      if (!uploadedFile) {
         return reply.status(400).send({ message: 'File is required' })
       }
 
-      await uploadImage({
+      const result = await uploadImage({
         fileName: uploadedFile.filename,
         contentType: uploadedFile.mimetype,
         contentStream: uploadedFile.file,
       })
 
-      await db.insert(schema.uploads).values({
-        name: 'testing 123.jpg',
-        remoteKey: 'aaa.jpg',
-        remoteUrl: `https://example.com/uploads/aaa.jpg`,
-      })
-      return reply.status(201).send({ uploadId: '12345' })
+      if (isRight(result)) {
+        return reply.status(201).send(null)
+      }
+
+      const error = unwrapEither(result)
+
+      switch (error.constructor.name) {
+        case 'InvalidFileFormatError':
+          return reply.status(400).send({ message: error.message })
+      }
     }
   )
 }
