8053b16881c8b0d3cf5a22ed608b02e32c20b2b2
Thais Cristine <this.cristine06@gmail.com>
2026-05-10 22:06:26 +0300

    Cloudflare R2 connection setup

--- src/env.ts
+++ src/env.ts
@@ -6,6 +6,12 @@ const envSchema = z.object({
     .enum(['development', 'test', 'production'])
     .default('development'),
   DATABASE_URL: z.string().url().startsWith('postgresql://'),
+
+  CLOUDFLARE_ACCOUNT_ID: z.string(),
+  CLOUDFLARE_ACCESS_KEY_ID: z.string(),
+  CLOUDFLARE_SECRET_ACCESS_KEY: z.string(),
+  CLOUDFLARE_BUCKET: z.string(),
+  CLOUDFLARE_PUBLIC_URL: z.string().url(),
 })
 
 export const env = envSchema.parse(process.env)
