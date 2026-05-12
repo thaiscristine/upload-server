bf09b351af5b76f053585b2569913899d52c626e
Thais Cristine <this.cristine06@gmail.com>
2026-05-12 09:19:35 +0300

    Create upload image tests

---
+++ src/app/functions/upload-image.spec.ts
@@ -0,0 +1,54 @@
+import { randomUUID } from 'node:crypto'
+import { Readable } from 'node:stream'
+import { eq } from 'drizzle-orm'
+import { beforeAll, describe, expect, it, vi } from 'vitest'
+import { db } from '@/infra/db'
+import { schema } from '@/infra/db/schemas'
+import { isLeft, isRight, unwrapEither } from '@/shared/either'
+import { InvalidFileFormatError } from './errors/invalid-file-format'
+import { uploadImage } from './upload-image'
+
+describe('upload image', () => {
+  beforeAll(() => {
+    vi.mock('@/infra/storage/upload-file-to-storage', () => ({
+      uploadFileToStorage: vi.fn().mockImplementation(() => {
+        return {
+          key: `${randomUUID()}-mocked-file.jpg`,
+          url: 'https://storage-example.com/mocked-key.jpg',
+        }
+      }),
+    }))
+  })
+
+  it('should upload an image and return the URL', async () => {
+    const fileName = `${randomUUID()}-mocked-file.jpg`
+
+    // system under test (sut)
+    const sut = await uploadImage({
+      fileName,
+      contentType: 'image/jpeg',
+      contentStream: Readable.from([]), // Mocked empty stream for testing
+    })
+
+    expect(isRight(sut)).toBe(true)
+
+    const result = await db
+      .select()
+      .from(schema.uploads)
+      .where(eq(schema.uploads.name, fileName))
+    expect(result).toHaveLength(1)
+  })
+
+  it('should not be able to upload an invalid file', async () => {
+    const fileName = `${randomUUID()}-mocked-file.pdf`
+
+    const sut = await uploadImage({
+      fileName,
+      contentType: 'document/pdf',
+      contentStream: Readable.from([]), // Mocked empty stream for testing
+    })
+
+    expect(isLeft(sut)).toBe(true)
+    expect(unwrapEither(sut)).toBeInstanceOf(InvalidFileFormatError)
+  })
+})
